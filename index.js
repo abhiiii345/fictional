e74d35ff1351df3af9a2b4774f79315900b77b05
abhiiii345 <128521977+abhiiii345@users.noreply.github.com>
2023-08-22 23:15:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector(".ph2").addEventListener("click", function(){
+    window.location.href="https://www.kemin.com/ap/en/markets/human-nutrition/products/floraglo-lutein-platforms/skin-health";
+})
+
+ 
